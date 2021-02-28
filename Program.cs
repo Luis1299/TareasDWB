64dcd504dcc16cb5b8767d71d370d2a2ca497236
Luis1299 <langelm1299@gmail.com>
2021-02-28 13:49:21 -0600

    Tarea 1: Select simple a la base de datos

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Tarea.Models;
+
+namespace Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindContext dbContext = new NorthwindContext();
+
+            // SELECT * FROM Products
+            var products = dbContext.Products.Select(p => p).ToList();
+
+            foreach(var product in products)
+            {
+                // Imprimir en consola el Id y el nombre del producto
+                Console.WriteLine(product.ProductId + " - "+ product.ProductName);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
